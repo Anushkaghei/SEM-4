7db264abfc80be87c16b97386083e08791649fb2
Anushka Ghei <79694271+Anushkaghei@users.noreply.github.com>
2023-01-24 13:13:30 +0530

    Create zigzag_matrix.c

---
+++ daa/lab/zigzag_matrix.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#define MAX_ROWS 10
+#define MAX_COLS 10
+
+void diagonal(int matrix[MAX_ROWS][MAX_COLS],int ROW, int COL)
+{
+    for(int i=0;i<=(ROW+COL);i++)
+    {
+        for(int j=0;j<COL;j++)
+        {
+            for(int k=0;k<ROW;k++)
+            {
+                if(k+j==i)
+                    printf("%d ",matrix[k][j]);
+            }
+        }
+    }
+}   
+
+int main() {
+    int matrix[MAX_ROWS][MAX_COLS];
+    int rows,cols;
+    scanf("%d",&rows);
+    scanf("%d",&cols);
+    for(int i=0; i<rows; i++){
+        for(int j=0; j<cols; j++){
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+    diagonal(matrix, rows, cols);
+    return 0;
+}
